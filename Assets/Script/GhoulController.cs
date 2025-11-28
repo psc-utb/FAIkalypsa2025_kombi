e9eff823270e70cfb97072163a0f751547110d3a
psc-utb <psc.utb@gmail.com>
2025-11-28 09:49:54 +0100

    Fyzika, RigidBody, Collidery, Skok

--- Assets/Script/GhoulController.cs
+++ Assets/Script/GhoulController.cs
@@ -6,16 +6,34 @@ public class GhoulController : MonoBehaviour
 {
     Animator animator;
 
+    Rigidbody rb;
+
     InputAction moveInputAction;
+    InputAction jumpInputAction;
 
     [SerializeField]
     float speed = 1;
+
+    bool isGrounded = false;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+
         moveInputAction = InputSystem.actions.FindAction("Move");
+        jumpInputAction = InputSystem.actions.FindAction("Jump");
+
+        jumpInputAction.performed += JumpInputAction_performed;
+    }
+
+    private void JumpInputAction_performed(InputAction.CallbackContext obj)
+    {
+        if (isGrounded)
+        {
+            rb.AddForce(0, 10000, 0);
+        }
     }
 
     // Update is called once per frame
@@ -31,12 +49,34 @@ public class GhoulController : MonoBehaviour
 
             Vector3 horizontalX = inputHorizontalX * Vector3.right;
             Vector3 horizontalZ = inputHorizontalZ * Vector3.forward;
-            Vector3 horizon = (horizontalX + horizontalZ) * speed * Time.deltaTime;
-            transform.Translate(horizon);
+            if (rb == null)
+            {
+                Vector3 horizon = (horizontalX + horizontalZ) * speed * Time.deltaTime;
+                transform.Translate(horizon);
+            }
+            else
+            {
+                rb.linearVelocity = (horizontalX + horizontalZ) * speed;
+            }
         }
         else
         {
             animator.SetBool("IsMoving", false);
         }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        isGrounded = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        isGrounded = false;
     }
 }
